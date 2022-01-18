cc12d10f828f8a9b937261c61fe82b460c5d4fa2
Ruben Cordero <ruben.cordero29@yahoo.com>
2022-01-17 19:54:29 -0600

    2nd push

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require("express");
+require('dotenv').config();
+
 
 const app = express();
 
 app.use(express.json());
 
 
+
 let port = process.env.PORT || 8000;
 
 app.get('/', function(req, res){
-  res.send("hello!");
+  res.send("hello!", process.env.name );
 })
 
 app.listen(port, function(){
